9e3600e7d00214d054a1d9a7368a36690856ce0a
Tommaso <tommaso.sassoli@stud.unifi.it>
2022-06-29 18:50:15 +0200

    settings page

---
+++ htdocs/js/menu.js
@@ -0,0 +1,14 @@
+$(document).ready(function(){
+  $(".toggle-menu").on("click", function() {
+
+    if ($('main').css('display') == 'none') {
+      $("main").fadeIn(300);
+      $(".settings").hide();
+    } else {
+      $(".settings").fadeIn(300);
+      $("main").hide();
+    }
+    return false;
+    
+  });
+});
