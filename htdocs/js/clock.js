cd6712f49da92aeeed6de48876d28849b7c35038
Tommaso <tommaso.sassoli@stud.unifi.it>
2022-07-01 12:28:19 +0200

    clock js

---
+++ htdocs/js/clock.js
@@ -0,0 +1,24 @@
+var months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+var days_of_week = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"]
+
+function bindClock() {
+  setInterval(updateClock, 1000);
+}
+
+function updateClock() {
+  const date = new Date();
+
+  const hour = date.getHours();
+  const min = date.getMinutes();
+
+  const day_w = date.getDay();
+
+  const day_m = date.getDate();
+  const month = date.getMonth();
+  const year = date.getFullYear();
+
+  $('#today-hour').text(hour);
+  $('#today-minute').text(min);
+  $('#today-day-week').text(days_of_week[day_w]);
+  $('#today-date').text(months[month] + ' ' + day_m + ' ' + year);
+}
